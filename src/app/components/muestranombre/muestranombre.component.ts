3a939365adc654c65f318c80d13a0f4c310044fd
luisjavierluna <javierlunaluis@gmail.com>
2023-08-15 14:27:41 -0600

    6. ngOnChanges

--- src/app/components/muestranombre/muestranombre.component.ts
+++ src/app/components/muestranombre/muestranombre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-muestranombre',
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class MuestranombreComponent implements OnInit {
+export class MuestranombreComponent implements OnInit, OnChanges {
+
+  @Input() nombre!: string
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+  }
 
   ngOnInit(): void {
   }
