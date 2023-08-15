ad153098cb49f318d8e4884e34f56fa2ac4be045
luisjavierluna <javierlunaluis@gmail.com>
2023-08-15 10:50:39 -0600

    3. Inicio de proyecto - LifeCycle

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { MuestranombreComponent } from './components/muestranombre/muestranombre.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Pagina1Component,
+    MuestranombreComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/muestranombre/muestranombre.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-muestranombre',
+  templateUrl: './muestranombre.component.html',
+  styles: [
+  ]
+})
+export class MuestranombreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
